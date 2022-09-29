adfadca8cd0389ae71803f1c181fdb09f390bb86
aismeg <iaskja20@gmai.com>
2022-09-29 22:17:16 +0300

    7th checkpoint

--- Edit/JavaScript/script.js
+++ Edit/JavaScript/script.js
@@ -1,8 +1,9 @@
-let title = 1;
+let title = "this is a title";
 let screens = 1;
 let screenPrice = 1;
 let rollback = 1;
 let fullPrice = 1;
 let adaptive = 1;
 
+console.log(title);
 alert('hamburger');
